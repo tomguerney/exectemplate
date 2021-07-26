5be4e998cdf1670318f652278e562f4b7d59eba9
Tom Guerney <tguerney@gmail.com>
2021-07-26 21:50:23 +1000

    wip

--- exectemplate.go
+++ exectemplate.go
@@ -1,8 +1,40 @@
 package exectemplate
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"text/template"
+
+	"github.com/mattn/go-shellwords"
+)
 
 // Exec executes
-func Exec(template string, values map[string]string) {
-	fmt.Println("template is " + template)
+func Exec(template string, values map[string]string) error {
+	command, err := interpolate(template, values)
+	if err != nil {
+		return err
+	}
+	fmt.Println(command)
+	return nil
+}
+
+func interpolate(tmpl string, values map[string]string) (string, error) {
+	builder := strings.Builder{}
+	parsed, err := template.New("stencil").Parse(tmpl)
+	if err != nil {
+		return "", err
+	}
+	err = parsed.Execute(&builder, values)
+	if err != nil {
+		return "", err
+	}
+	return builder.String(), nil
+}
+
+func sliceArgs(args string) ([]string, error) {
+	slice, err := shellwords.Parse(args)
+	if err != nil {
+		return nil, err
+	}
+	return slice, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tomguerney/exectemplate
 
 go 1.16
+
+require github.com/mattn/go-shellwords v1.0.12
